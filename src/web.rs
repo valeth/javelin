a6d8ce55b38adbdd767082318cebdd121405eb83
Patrick Auernig <patrick.auernig@mykolab.com>
2019-01-13 16:23:04 +0100

    Add small JSON API for querying active streams

--- src/web.rs
+++ src/web.rs
@@ -2,6 +2,7 @@ use std::{
     thread,
     net::SocketAddr,
 };
+use serde_json::json;
 use warp::{
     self,
     Filter,
@@ -36,5 +37,34 @@ fn server(shared: Shared) {
 
     let hls_files = warp::path("hls").and(warp::fs::dir(hls_root));
 
-    warp::serve(hls_files).run(addr);
+    let streams_api = warp::path("api").and(warp::path::param())
+        .map(move |resource: String| {
+            let json = match resource.as_str() {
+                "active_streams" => {
+                    json!({ "streams": active_streams(&shared) })
+                }
+                _ => {
+                    json!({ "error": "Unknown resource" })
+                }
+            };
+
+            warp::reply::json(&json)
+        });
+
+    let routes = hls_files.or(streams_api);
+
+    warp::serve(routes).run(addr);
+}
+
+fn active_streams(shared: &Shared) -> Vec<String> {
+    let streams = shared.streams.read();
+    streams.iter()
+        .filter_map(|(k, v)| {
+            if v.has_publisher() {
+                Some(k.clone())
+            } else {
+                None
+            }
+        })
+        .collect()
 }
