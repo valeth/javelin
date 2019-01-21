0ff7f2e7ecc94b6eabb32d80a742d9d9755ba7e6
Patrick Auernig <patrick.auernig@mykolab.com>
2019-01-21 18:56:17 +0100

    Implement error handling for webserver

--- src/web.rs
+++ src/web.rs
@@ -1,13 +1,39 @@
 use std::{
     thread,
     net::SocketAddr,
+    error::Error as StdError,
+    fmt::{self, Display},
 };
 use serde_json::json;
 use warp::{
     self,
     Filter,
+    Rejection,
+    Reply,
+    http::StatusCode,
+    filters::BoxedFilter,
 };
 use crate::Shared;
+
+
+#[derive(Clone, Debug)]
+enum ApiError {
+    NoSuchResource,
+}
+
+impl StdError for ApiError {
+    fn description(&self) -> &str {
+        match *self {
+            ApiError::NoSuchResource => "No such resource"
+        }
+    }
+}
+
+impl Display for ApiError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.description())
+    }
+}
 
 
 pub struct WebServer {
@@ -26,7 +52,6 @@ impl WebServer {
 }
 
 
-#[allow(clippy::needless_pass_by_value)] // shut up
 fn server(shared: Shared) {
     let addr: SocketAddr = "0.0.0.0:8080".parse().unwrap();
 
@@ -35,23 +60,15 @@ fn server(shared: Shared) {
         config.hls.root_dir.clone()
     };
 
-    let hls_files = warp::path("hls").and(warp::fs::dir(hls_root));
+    let hls_files = warp::path("hls")
+        .and(warp::fs::dir(hls_root));
 
-    let streams_api = warp::path("api").and(warp::path::param())
-        .map(move |resource: String| {
-            let json = match resource.as_str() {
-                "active_streams" => {
-                    json!({ "streams": active_streams(&shared) })
-                }
-                _ => {
-                    json!({ "error": "Unknown resource" })
-                }
-            };
+    let streams_api = warp::path("api")
+        .and(api(shared.clone()));
 
-            warp::reply::json(&json)
-        });
-
-    let routes = hls_files.or(streams_api);
+    let routes = hls_files
+        .or(streams_api)
+        .recover(error_handler);
 
     warp::serve(routes).run(addr);
 }
@@ -67,4 +84,33 @@ fn active_streams(shared: &Shared) -> Vec<String> {
             }
         })
         .collect()
+}
+
+fn api(shared: Shared) -> BoxedFilter<(impl Reply,)> {
+    warp::path("active-streams")
+        .map(move || {
+            let json = json!({
+                "streams": active_streams(&shared)
+            });
+            warp::reply::json(&json)
+        })
+        .or_else(|_err| {
+            Err(warp::reject::custom(ApiError::NoSuchResource))
+        })
+        .boxed()
+}
+
+fn error_handler(err: Rejection) -> Result<impl Reply, Rejection> {
+    match err.find_cause::<ApiError>() {
+        Some(e @ ApiError::NoSuchResource) => {
+            let code = StatusCode::NOT_FOUND;
+            let json = json!({
+                "code": code.as_u16(),
+                "error": e.description()
+            });
+            let reply = warp::reply::json(&json);
+            Ok(warp::reply::with_status(reply, code))
+        },
+        None => Err(err)
+    }
 }
